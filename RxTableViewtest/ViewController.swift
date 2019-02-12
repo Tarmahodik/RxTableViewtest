5945e7ddd71f7a2dff4e30d954030cc8e2ece80c
Георгий Ходиков <100007084663795@facebook.com>
2019-02-12 17:13:53 +0300

    Initial Commit

---
+++ RxTableViewtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxTableViewtest
+//
+//  Created by Георгий Ходиков on 12/02/2019.
+//  Copyright © 2019 Георгий Ходиков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
